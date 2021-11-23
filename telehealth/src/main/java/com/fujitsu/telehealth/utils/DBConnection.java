c8283b1cb500817f9680b73603acb5cd9d585ef4
Rene <devprog32@gmail.com>
2021-11-23 19:45:03 +0800

    UPDATE, PULL BEFORE PUSH

--- telehealth/src/main/java/com/fujitsu/telehealth/utils/DBConnection.java
+++ telehealth/src/main/java/com/fujitsu/telehealth/utils/DBConnection.java
@@ -3,7 +3,6 @@ package com.fujitsu.telehealth.utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class DBConnection {
 	
