3eb20322f474248fb015a421caa713e62a1f55d9
Rene <devprog32@gmail.com>
2021-11-25 04:07:10 +0800

    TEST

--- telehealth/src/main/java/com/fujitsu/telehealth/model/SendMail.java
+++ telehealth/src/main/java/com/fujitsu/telehealth/model/SendMail.java
@@ -2,7 +2,10 @@ package com.fujitsu.telehealth.model;
 
 import java.util.Properties;
 
+import javax.mail.Authenticator;
 import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
