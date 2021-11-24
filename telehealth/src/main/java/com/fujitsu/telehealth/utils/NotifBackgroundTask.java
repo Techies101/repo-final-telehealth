3b29fb2f44fc9ec21f384601c1f7a341ed2a0b4c
Rene <devprog32@gmail.com>
2021-11-25 00:27:41 +0800

    Remove Login code redunduncy

--- telehealth/src/main/java/com/fujitsu/telehealth/utils/NotifBackgroundTask.java
+++ telehealth/src/main/java/com/fujitsu/telehealth/utils/NotifBackgroundTask.java
@@ -1,49 +1,17 @@
 package com.fujitsu.telehealth.utils;
 
-import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-import org.quartz.Job;
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.SimpleScheduleBuilder;
-import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
-import org.quartz.impl.StdSchedulerFactory;
-
 import com.fujitsu.telehealth.dao.AppPatientImplementation;
 import com.fujitsu.telehealth.model.PatientModel;
 
-public class NotifBackgroundTask extends SQLQuery implements Job {
+public class NotifBackgroundTask  {
 
 	AppPatientImplementation App = new AppPatientImplementation();
 	PatientModel patient = new PatientModel();
-
-	public static void main(String args[]) throws SchedulerException {
-
-		JobDetail job = JobBuilder.newJob(NotifBackgroundTask.class).build();
-
-//		Trigger t1 = TriggerBuilder.newTrigger().withIdentity("AppointmentTrigger").startNow().build();
-//		Trigger t2 = TriggerBuilder.newTrigger().withIdentity("CronTrigger").withSchedule(CronScheduleBuilder.cronSchedule("0 0/1 * 1/1 * ? *")).build();
-		
-		Trigger t3 = TriggerBuilder.newTrigger().withIdentity("CronTrigger")
-				.withSchedule(SimpleScheduleBuilder
-						.simpleSchedule()
-						.withIntervalInSeconds(05)
-						.repeatForever()).build();
-		
-		Scheduler sc = StdSchedulerFactory.getDefaultScheduler();
-
-		sc.start();
-		sc.scheduleJob(job, t3);
-	}
 
 	public static Boolean getTimeDiff(String th_time, String th_date_shcedule) throws ParseException {
 
@@ -66,16 +34,6 @@ public class NotifBackgroundTask extends SQLQuery implements Job {
 
 		invokeReminder = (actDiff <= 2 && actDiff >= 0 && th_date_shcedule.equals(df.format(new Date()))) ? true : false;
 		return invokeReminder;
-	}
-
-	@Override
-	public void execute(JobExecutionContext arg0) throws JobExecutionException {
-		try {
-			if (App.getSchedule(patient.getTh_patientID()) != null) return;
-			System.out.println(new Date());
-		} catch (SQLException | ParseException e) {
-			e.printStackTrace();
-		}
 	}
 
 }
